8ca856626231e0c19c84f321a95d3ef39c997dd3
[hiberapp] <[mdriyaz147@gmail.com]>
2017-10-12 23:37:34 -0400

    modified:   src/app/app.component.html 	modified:   src/app/app.component.spec.ts 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts
    	src/app/box.component.ts
    	src/app/box.ts
    	src/app/boxtype.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BoxType } from './boxtype';
+import { Box } from './box';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  boxes: Array<Box> = [{text: 'Box 1', color: BoxType.RED},
+    {text: 'Box 2', color: BoxType.RED},
+    {text: 'Box 3', color: BoxType.RED},
+    {text: 'Box 4'}
+  ];
 }
